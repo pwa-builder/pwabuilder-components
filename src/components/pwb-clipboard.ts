73f224b95bda3b076ce1b9d5919350eb6daf62f3
Justin Willis <jgw9617@gmail.com>
2019-08-06 23:30:21 -0700

    feat(): add clipboard component

--- src/components/pwb-clipboard.ts
+++ src/components/pwb-clipboard.ts
@@ -2,27 +2,53 @@ import {
   LitElement, html, customElement, property
 } from 'lit-element';
 
-/**
- * Use the customElement decorator to define your class as
- * a custom element. Registers <my-element> as an HTML tag.
- */
 @customElement('pwb-clipboard')
 export class pwbclipboard extends LitElement {
 
-  /**
-   * Create an observed property. Triggers update on change.
-   */
   @property()
-  foo = 'foo';
+  textToCopy: string;
 
-  /**
-   * Implement `render` to define a template for your element.
-   */
+  public async copyText(): Promise<void> {
+    if (navigator.clipboard) {
+      try {
+        if (this.textToCopy) {
+          await navigator.clipboard.writeText(this.textToCopy);
+        }
+        else {
+          console.info("pwb-clipboard: You must pass the property textToCopy. Something like <pwb-clipboard texttocopy='hello world' />");
+        }
+      }
+      catch (err) {
+        console.error(err);
+      }
+    }
+  }
+
+  public async readText(): Promise<string> {
+    if (navigator.clipboard) {
+      try {
+        const clipboardText = await navigator.clipboard.readText();
+
+        if (clipboardText) {
+          return clipboardText;
+        }
+        else {
+          return null;
+        }
+      }
+      catch (err) {
+        console.error(err);
+      }
+    }
+  }
+
   render() {
     /**
      * Use JavaScript expressions to include property values in
      * the element template.
      */
-    return html`<p>${this.foo}</p>`;
+    return html`
+      <button @click="${() => this.copyText()}">Copy</button>
+    `;
   }
 }
